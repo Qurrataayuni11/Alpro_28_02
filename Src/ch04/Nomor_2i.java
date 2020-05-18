e0c8397332a4a36469eea3d24d578284d695da33
Qurrataayuni11 <62864898+Qurrataayuni11@users.noreply.github.com>
2020-05-18 14:06:59 +0700

    Update Nomor_2i.java

--- Src/ch04/Nomor_2i.java
+++ Src/ch04/Nomor_2i.java
@@ -1,17 +1,56 @@
 package ch04;
+import java.util.Scanner;
 
 public class Nomor_2i {
   public static void main (String[] args) {
 	int[]nilai = {82,12,41,38,19,26,9,48,20,55,8,32,3};
-	
-	System.out.println("Menampilkan Selisih Angka dengan Angka Setelahnya: ");
-	for (int i = 0; i<nilai.length; i++){
-	    if (nilai[i]-1 <= 13) {
-	    System.out.print(nilai[i] +" ");
+	int x = 0;
+
+	for (int i = 0; i<13; i++){
+	    x = i+1;
+		
+	    System.out.print("(");
+	    System.out.print(nilai[i]-nilai[x] +"), ");
 
 
-	   }
-        }
+	}
    } 
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
